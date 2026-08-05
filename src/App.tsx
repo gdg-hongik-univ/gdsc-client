4e216d742c0cea1a4b1f318f4351434f261ab95a
소울치킨 <soulfever01@naver.com>
2026-08-05 11:26:17 +0900

    feat: GA4 삽입
    - 페이지 경로마다 이벤트 전송

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,29 @@ import { media } from '@/styles';
 import { isAuthenticated } from '@/utils/auth';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { color, typography } from 'wowds-tokens';
 import Button from 'wowds-ui/Button';
 
 import Carousel from './components/onboarding/Carousel';
 import RoutePath from './routes/routePath';
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_ID || 'G-XXXXXXXXXX';
+ReactGA.initialize(GA_MEASUREMENT_ID);
 
 function App() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: location.pathname + location.search
+    });
+  }, [location]);
 
   return (
     <Wrapper direction="column" justify="flex-start" align="center">
