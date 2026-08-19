4957b033410935355983c56c7e333b7b857d293e
[mminn94] <joykim0922@gmail.com>
2026-08-20 05:08:44 +0900

    feat: 재학생 인증 라우트 및 페이지 export 추가

--- src/routes/routePath.ts
+++ src/routes/routePath.ts
@@ -13,16 +13,17 @@ const RoutePath = {
 
   GithubSignin: '/auth',
   StudentVerification: '/student-verification',
+  StudentVerificationCode: '/student-verification/code',
   Signup: '/signup',
   EmailVerification: '/email-verification',
 
   AuthGithubLoginRedirect: `${BASE_URL}/oauth2/authorization/github`,
 
   AuthServerRedirect: '/social-login/redirect',
-  StudentVerificationServerRedirect: '/onboarding/verify-univ-email',
-  EmailVerificationServerRedirect: '/onboarding/verify-email',
-  GDGHongikLink: 'https://www.gdghongik.com',
-  GDGHongikDiscord: 'https://discord.gg/dSV6vSEuGU',
+  // StudentVerificationServerRedirect: '/onboarding/verify-univ-email',
+  // EmailVerificationServerRedirect: '/onboarding/verify-email',
+  GDSCHongikLink: 'https://www.gdghongik.com',
+  GDSCHongikDiscord: 'https://discord.gg/dSV6vSEuGU',
   GitHubGuideLink: 'https://www.gdghongik.com/onboard-guide/github',
   StudentEmailLinkGuideLink:
     'https://www.gdghongik.com/onboard-guide/student-email',
