7ef13cb15cebf6c38792588e8d58543eb3ce7bf0
소울치킨 <soulfever01@naver.com>
2026-08-25 12:03:13 +0900

    fix: 깃허브 계정 변경관련 컴포넌트 주석처리

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -44,7 +44,10 @@ export const SignUp = () => {
   const { createInfo } = useCreateUserInfo();
   const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [previousStudentId, setPreviousStudentId] = useState('');
+
+  // TODO: 깃허브 계정 변경 기능이 되면 돌려놓기
+  // const [previousStudentId, setPreviousStudentId] = useState('');
+  const [previousStudentId] = useState('');
 
   useGetAccountInfo();
   const currentGithubHandle = useAccountInfoStore(
@@ -96,12 +99,11 @@ export const SignUp = () => {
   }, [isPreviousMemberInfoLoading, previousMemberInfo, previousStudentId]);
 
   // TODO : 2차 모집때 다시 풀어줄 예정
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleRegisterClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setPreviousStudentId(studentId);
-    // 모달은 previousMemberInfo 로딩 완료 후 useEffect에서 열림
-  };
+  // const handleRegisterClick = (e: React.MouseEvent) => {
+  //   e.preventDefault();
+  //   setPreviousStudentId(studentId);
+  //   // 모달은 previousMemberInfo 로딩 완료 후 useEffect에서 열림
+  // };
 
   const handleModalClose = () => {
     setIsModalOpen(false);
@@ -505,17 +507,18 @@ const HelperTextWrapper = styled.div`
   gap: 4px;
 `;
 
-const RegisterButton = styled.a`
-  color: inherit;
-  text-decoration: underline;
-  cursor: pointer;
-  &:hover {
-    opacity: 0.8;
-  }
-  &:visited {
-    color: inherit;
-  }
-`;
+// 2차 모집때 다시 풀어줄 예정
+// const RegisterButton = styled.a`
+//   color: inherit;
+//   text-decoration: underline;
+//   cursor: pointer;
+//   &:hover {
+//     opacity: 0.8;
+//   }
+//   &:visited {
+//     color: inherit;
+//   }
+// `;
 
 const ModalTitle = styled(Text)`
   ${typography.body1}
